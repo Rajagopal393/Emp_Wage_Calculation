c6b624e622e47c87551e7bca05102d68dbb7cd39
Raj Gopal <harirajagopal393@gmail.com>
2020-04-11 15:19:31 +0530

    Added Feature to Keep A Limit Of Working Time Of An Employee

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -3,12 +3,17 @@ isAbsent=0;
 isPresent=1;
 isPartTime=2;
 workingHours=0;
+month=30
 workingDaysInMonth=20
-for (( DaysPassed=1; DaysPassed <= $workingDaysInMonth; DaysPassed++ ))
+presentDays=0
+maxHours=100
+maxDays=20
+for (( DaysPassed=1; DaysPassed <= $month; DaysPassed++ ))
 do
     empStatus=$((RANDOM%3));
     case $empStatus in
         $isPresent)
+            presentDays=$(($presentDays+1))
             workingHours=$(($workingHours+ 8))
             ;;
         $isAbsent)
@@ -18,7 +23,12 @@ do
             workingHours=$(($workingHours+ 4))
             ;;
     esac
+    if [ $workingHours -gt $maxHours ] || [ $presentDays -gt $maxDays ]
+        then
+            echo "limit reached...give him rest"
+            break
+    fi
 done
 wagePerHour=20;
 totalDailyWage=$(($wagePerHour*$workingHours));
-echo "$totalDailyWage";
+echo "$totalDailyWage is employee's earning ";
