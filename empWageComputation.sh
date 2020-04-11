e689a55bac02441c37a3f008554151fdb76f90f6
Raj Gopal <harirajagopal393@gmail.com>
2020-04-11 14:52:34 +0530

    Added Employee Daily Wage Calculation Feature

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -2,8 +2,13 @@
 isPresent=1
 empStatus=$((RANDOM%2))
 if [ $isPresent -eq $empStatus ]
-    then 
+then 
         echo "employee is present"
 else
         echo "employee is absent"
 fi
+
+wagePerHour=20
+noOfHoursinDay=8
+totalDailyWage=$(($wagePerHour*$noOfHoursinDay))
+echo "$totalDailyWage"
